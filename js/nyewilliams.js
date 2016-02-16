052a202b99c573c241ae6d3da6523d444be05a13
joehalloran <joestandard@Mac-Pro-Joe.local>
2016-02-16 09:18:45 +0000

    redesign

--- js/nyewilliams.js
+++ js/nyewilliams.js
@@ -3,18 +3,12 @@ $('.film-preview-item').children('a').on('click', function ( e ) {
 	$( this ).parent().siblings().each( function () {
 		$( this ).children('.film-text').hide();
 	});
-	$(this).siblings('.film-text').show().addClass("film-text-visible");
+	$(this).siblings('.film-text').toggleClass("film-text-visible").toggle();
 	$('.film-text-visible').mouseleave(function() {
 		$( this ).hide().removeClass("film-text-visible");
 	});
 
 });
 
-$('.film-text-close').on('click', function ( e ){
-	e.preventDefault();
-	$( this ).parent().hide();
-});
 
-$(window).mousemove(function () {
-	$('.film-text-close').hide();
-});
+
