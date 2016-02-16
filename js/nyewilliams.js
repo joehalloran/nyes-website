8425af23e0b55c47223adb549ba71cbef67bf6df
joehalloran <joestandard@Mac-Pro-Joe.local>
2016-02-16 10:50:34 +0000

    adding flex-box align

--- js/nyewilliams.js
+++ js/nyewilliams.js
@@ -1,9 +1,8 @@
-$('.film-preview-item').children('a').on('click', function ( e ) {
-	e.preventDefault();
-	$( this ).parent().siblings().each( function () {
+$('.film-preview-item').on('click', function ( e ) {
+	$( this ).siblings().each( function () {
 		$( this ).children('.film-text').hide();
 	});
-	$(this).siblings('.film-text').toggleClass("film-text-visible").toggle();
+	$(this).children('.film-text').toggle().addClass("film-text-visible");
 	$('.film-text-visible').mouseleave(function() {
 		$( this ).hide().removeClass("film-text-visible");
 	});
