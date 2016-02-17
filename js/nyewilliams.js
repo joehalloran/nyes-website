a8155e6da61b2a684838fd9e14c18dd9befb9500
joehalloran <joestandard@Mac-Pro-Joe.local>
2016-02-17 11:43:56 +0000

    fix of js
    using class ‘film-text-show’ instead of hide() or show() to avoid
    conflict with display: flex in css

--- js/nyewilliams.js
+++ js/nyewilliams.js
@@ -1,17 +1,17 @@
-$('.film-preview-item').on('click', function ( e ) {
-	$( this ).siblings().each( function () {
-		$( this ).children('.film-text').hide().removeAttr('style');
+$(function () {
+	// this script uses '.film-text-show' to control visibility of film text (see css file style.css)
+	var $allFilmBoxes =	$('.film-preview-item'); //get all film boxes
+	$allFilmBoxes.on('click', function ( e ) {
+		var $this = $( this );
+		$this.siblings().each( function () {
+			$( this ).removeClass('film-text-show'); //hide any other boxes that are visible
+		});
+		$this.toggleClass('film-text-show'); //show if hidden or vice versa.
 	});
-	$(this).children('.film-text').toggle();
-	$('.film-preview-item').mouseleave(function() {
-		$( '.film-text' ).each( function () { $(this).removeAttr('style');});
+	$allFilmBoxes.mouseleave(function() { //safety function to ensure mousehover doesn't conflict with click
+		$allFilmBoxes.each( function () { 
+			$(this).removeClass('film-text-show'); //hide all if mouse leaves any
+		});
 	});
-
 });
-
-$('.modal').on('hidden.bs.modal', function (e) {
-	$('.film-text').removeAttr( 'style' );
-});
-
-
 
