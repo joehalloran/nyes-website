c5a1a0f1f6aa95dfd11e91aa581f1319600aae66
joehalloran <admin@administrators-MacBook-Pro.local>
2016-03-23 22:20:01 +0000

    js

--- js/nyewilliams.js
+++ js/nyewilliams.js
@@ -14,27 +14,37 @@ $(function () {
 		});
 	});
 
-	// $('.modal').each( function() {
-	// 	$( this ).on('shown.bs.modal', function () {
-	// 		console.log("Shown");
-	// 	});
-	// });
-	// $('.modal').on('show.bs.modal', function () {
-	// 	console.log("Show");
-	// 	var iframe = $(this).children('.embed-responsive');
-	// 	// $f == Froogaloop
-	// 	var player = $f(iframe);
+	// Listen for the ready event for any vimeo video players on the page
+    var vimeoPlayers = document.querySelectorAll('iframe'),
+        player;
+        
+    for (var i = 0, length = vimeoPlayers.length; i < length; i++) {
+        player = vimeoPlayers[i];
+        $f(player).addEvent('ready', ready);
+    }
+    /**
+     * Called once a vimeo player is loaded and ready to receive
+     * commands.
+     */
+    function ready(player_id) {
+        // Keep a reference to Froogaloop for this player
+        var container = document.getElementById(player_id).parentElement.parentElement,
+            froogaloop = $f(player_id);
+        /**
+         * Sets up the actions for the buttons that will perform simple
+         * api calls to Froogaloop.
+         */
+        function setupPauseButton() {
+        	// Setup pause button
+            var $pauseBtn = $('.close-modal');
 
-	// 	$('.modal').on('hidden.bs.modal', function () {
-	// 		console.log("CLOSE");
-	// 		player.api('pause');
-	// 	});
-	// });
-	
-
-	// $('.modal').on('shown.bs.modal', function () {
-	// 	player.api('play');
-	// });
+            // Call pause when pause button clicked
+            $pauseBtn.click( function() {
+                froogaloop.api('pause');
+            });
+        }
+        setupPauseButton();
+    }
 
 });
 
